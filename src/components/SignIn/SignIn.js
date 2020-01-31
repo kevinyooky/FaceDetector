83f3d362b9f693d47f635d3d0f239df0e1f8ff00
Kevin Yoo <kevinyooky@gmail.com>
2020-01-31 08:16:24 -0800

    demo

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,26 +1,26 @@
 import React from "react";
 
-class SignIn extends React.Component{
-  constructor(props){
-    super(props)
-    this.state={
-      signInEmail:'',
-      signInPassword:''
-    }
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: ""
+    };
   }
 
-  onEmailChange = (e) => {
-    this.setState({signInEmail: e.target.value})
-  }
+  onEmailChange = e => {
+    this.setState({ signInEmail: e.target.value });
+  };
 
-  onPasswordChange = (e) =>{
-    this.setState({signInPassword:e.target.value})
-  }
+  onPasswordChange = e => {
+    this.setState({ signInPassword: e.target.value });
+  };
 
-  onSubmitSignIn = () =>{
-    fetch('https://lit-falls-23672.herokuapp.com/signin', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+  onSubmitSignIn = () => {
+    fetch("https://lit-falls-23672.herokuapp.com/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: this.state.signInEmail,
         password: this.state.signInPassword
@@ -28,17 +28,16 @@ class SignIn extends React.Component{
     })
       .then(response => response.json())
       .then(user => {
-        if(user.id){
-          this.props.loadUser(user)
-          this.props.onRouteChange('home')
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
         }
-        console.log('user', user)
-      })
-    
-  }
+        console.log("user", user);
+      });
+  };
 
-  render(){
-    const {onRouteChange} = this.props
+  render() {
+    const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
@@ -46,6 +45,8 @@ class SignIn extends React.Component{
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
+                <p>Demo Email: demo@gmail.com</p>
+                <p>Demo Password: demo</p>
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">
                   Email
                 </label>
@@ -54,7 +55,7 @@ class SignIn extends React.Component{
                   type="email"
                   name="email-address"
                   id="email-address"
-                  onChange= {this.onEmailChange}
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -66,7 +67,7 @@ class SignIn extends React.Component{
                   type="password"
                   name="password"
                   id="password"
-                  onChange= {this.onPasswordChange}
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
@@ -79,16 +80,19 @@ class SignIn extends React.Component{
               />
             </div>
             <div className="lh-copy mt3">
-              <p onClick={()=>onRouteChange('register')} href="#0" className="f6 link dim black db pointer">
+              <p
+                onClick={() => onRouteChange("register")}
+                href="#0"
+                className="f6 link dim black db pointer"
+              >
                 Register
               </p>
             </div>
-          </div> 
+          </div>
         </main>
       </article>
     );
   }
-  
-};
+}
 
 export default SignIn;
